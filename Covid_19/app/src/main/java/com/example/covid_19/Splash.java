ea80791c9ed4c414b52071d5bf5d72a1800400e1
sanjayaSandaruwan <64325760+sanjayaSandaruwan@users.noreply.github.com>
2020-04-26 11:24:09 +0530

    mmm

--- Covid_19/app/src/main/java/com/example/covid_19/Splash.java
+++ Covid_19/app/src/main/java/com/example/covid_19/Splash.java
@@ -2,13 +2,45 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Splash extends AppCompatActivity {
+
+ProgressBar progressBar;
+TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        progressBar=findViewById(R.id.progress);
+        textView=findViewById(R.id.txt_count);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        prograssAnimation();
+
+
+
+    }
+
+    public void prograssAnimation(){
+        PogressBArAnimation anim=new PogressBArAnimation(this,progressBar,textView,0f,100f);
+        anim.setDuration(8000);
+        progressBar.setAnimation(anim);
     }
 }
