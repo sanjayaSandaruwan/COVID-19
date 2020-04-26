c2caa402a6213bde9fb973126bba1b2f4e6b5958
sanjayaSandaruwan <64325760+sanjayaSandaruwan@users.noreply.github.com>
2020-04-26 08:34:29 +0530

    email
    email part

--- Covid_19/app/src/main/java/com/example/covid_19/About.java
+++ Covid_19/app/src/main/java/com/example/covid_19/About.java
@@ -3,13 +3,17 @@ package com.example.covid_19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
     FrameLayout frameLayout;
+TextView email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,18 @@ public class About extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         frameLayout=findViewById(R.id.btn_go);
+        email=findViewById(R.id.txt_mail_address);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("mailto:" + "ssandaruwan57@gmail.com"));
+            intent.putExtra(Intent.EXTRA_SUBJECT,"");
+            intent.putExtra(Intent.EXTRA_TEXT,"");
+
+                startActivity(intent);
+            }
+        });
 
         frameLayout.setOnClickListener(new View.OnClickListener() {
             @Override
